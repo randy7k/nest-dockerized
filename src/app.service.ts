bb98ec1f9c726b9f3ec34cbfec8ce0d3436e6b52
Randy Rivera <randyr7k@gmail.com>
2022-11-08 14:38:55 -0400

    Dockerized it

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const test = process.env.TEST
+    return 'Hello World!' + test;
   }
 }
